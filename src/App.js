ce5744f6324e6f86c496e2e35e2dea31be05e50c
Rishabh parihar <prishabh9@gmail.com>
2022-08-30 16:27:34 +0530

    commit 9

--- src/App.js
+++ src/App.js
@@ -31,12 +31,18 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      {categories.map(({ title, id }) => (
+      {categories.map(({ title, id, imageUrl }) => (
         <div key={id} className="category-container">
-          {/* <img/> */}
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          >
+            <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
           </div>
         </div>
       ))}
