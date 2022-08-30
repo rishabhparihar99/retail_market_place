4a82f57fcf584aa8b983747f218ead19115dc957
Rishabh parihar <prishabh9@gmail.com>
2022-08-30 15:03:59 +0530

    commit 8 images

--- src/App.js
+++ src/App.js
@@ -3,24 +3,29 @@ import "./categories.styles.scss";
 const App = () => {
   const categories = [
     {
-      title: "Hats",
       id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
-      title: "Jackets",
       id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
-      title: "Sneakers",
       id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
-      title: "Womens",
       id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
-      title: "Mens",
       id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
 
