933d9b9f6e431234dbe1b7e895ce59c6ad351eaf
Rishabh parihar <prishabh9@gmail.com>
2022-09-01 17:35:56 +0530

    Commit 28 Firebase Config

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0lwCdWVd1zV0BGgk2L3rw60BjTzrCHpk",
+  authDomain: "crwn-clothing-db-c679c.firebaseapp.com",
+  projectId: "crwn-clothing-db-c679c",
+  storageBucket: "crwn-clothing-db-c679c.appspot.com",
+  messagingSenderId: "831602955885",
+  appId: "1:831602955885:web:cff8f5494df887c734d164",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
