10ee3a5e825627160e431ef0fc72eaf023fc3582
Rishabh parihar <prishabh9@gmail.com>
2022-09-01 15:31:10 +0530

    Commit 22 Link Component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,15 @@
-import {Outlet} from 'react-router-dom';
+import {Outlet,Link} from 'react-router-dom';
 import {Fragment} from 'react';
 const Navigation = () => {
     return (
       <Fragment>
         <div className='navigation'>
-          <h1>I am navigation bar</h1>
+          <div>Logo</div>
+          <div className='links-container'>
+            <Link className='nav-link' to="/shop">
+                 SHOP
+            </Link>
+          </div>
         </div>
         <Outlet />
     </Fragment>
