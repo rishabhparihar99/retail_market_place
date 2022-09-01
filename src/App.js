065dd63f8ef57c968b502ea7901d38add9920ce7
Rishabh parihar <prishabh9@gmail.com>
2022-09-01 14:41:06 +0530

    Commit 17 Nested Routing Problem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
+
+const Shop = () => {
+  return <h1>I am at the shop pages</h1>;
+};
+
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />}>
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
