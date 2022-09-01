fada8542cedf4ec8ffdb018f553f8b2a264f69ca
Rishabh parihar <prishabh9@gmail.com>
2022-09-01 14:29:31 +0530

    Commit 16 Home Page Routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import Home from "./routes/home/home.component";
-
+import { Routes, Route } from "react-router-dom";
 const App = () => {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 };
 export default App;
